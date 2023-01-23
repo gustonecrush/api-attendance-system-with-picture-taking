8e19c3133793d676ad041464eb89527b92528a77
gustonecrush <codeboxezz@gmail.com>
2023-01-23 15:05:16 +0700

    created register function

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,44 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->messages(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'is_admin' => 0,
+        ]);
+
+        if ($user) {
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json(
+                [
+                    'data' => $user,
+                    'access_token' => $token,
+                    'token_type' => 'Bearer',
+                ],
+                200
+            );
+        } else {
+            return response()->json(['message' => 'Register is Failed'], 400);
+        }
+    }
 }
