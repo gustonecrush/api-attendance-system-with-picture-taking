42c3c9cd6680a0e034d573396d3547dc9b5295cc
gustonecrush <codeboxezz@gmail.com>
2023-01-23 16:20:24 +0700

    auth controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -42,5 +43,31 @@ class AuthController extends Controller
         } else {
             return response()->json(['message' => 'Register is Failed'], 400);
         }
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => "Hi $user->name welcome to home",
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::user()
+            ->tokens()
+            ->delete();
+            
+        return $this->sendResponse(['message' => 'Logout successfully'], 200);
     }
 }
