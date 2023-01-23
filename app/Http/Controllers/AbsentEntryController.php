7af367387810c646c22580eb3007283423e831f1
gustonecrush <codeboxezz@gmail.com>
2023-01-23 14:28:54 +0700

    created controller, model, migration, resource, and seeder

---
+++ app/Http/Controllers/AbsentEntryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AbsentEntry;
+use Illuminate\Http\Request;
+
+class AbsentEntryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AbsentEntry  $absentEntry
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AbsentEntry $absentEntry)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AbsentEntry  $absentEntry
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AbsentEntry $absentEntry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AbsentEntry  $absentEntry
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AbsentEntry $absentEntry)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AbsentEntry  $absentEntry
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AbsentEntry $absentEntry)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/AbsentOutController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AbsentOut;
+use Illuminate\Http\Request;
+
+class AbsentOutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AbsentOut  $absentOut
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AbsentOut $absentOut)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AbsentOut  $absentOut
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AbsentOut $absentOut)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AbsentOut  $absentOut
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AbsentOut $absentOut)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AbsentOut  $absentOut
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AbsentOut $absentOut)
+    {
+        //
+    }
+}
