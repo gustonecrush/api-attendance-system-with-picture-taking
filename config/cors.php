c12c65322de501acf717f115345e716877d9a01b
gustonecrush <codeboxezz@gmail.com>
2023-03-01 23:55:57 +0700

    modified cors

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     /*
      * You can enable CORS for 1 or multiple paths.
      */
-    'paths' => ['*', 'api/*'],
+    'paths' => ['*'],
 
     /*
     * Matches the request method. `['*']` allows all methods.
